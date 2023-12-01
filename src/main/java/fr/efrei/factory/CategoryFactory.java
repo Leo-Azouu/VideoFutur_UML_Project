8a9c438b488a76dd860c18ff8e518a646bcad0aa
Leo-Azouu <104171733+Leo-Azouu@users.noreply.github.com>
2023-12-01 18:56:36 +0100

    UpdateCategoryFactory
    Few update on CategoryFactory

--- src/main/java/fr/efrei/factory/CategoryFactory.java
+++ src/main/java/fr/efrei/factory/CategoryFactory.java
@@ -1,5 +1,16 @@
 package fr.efrei.factory;
+import fr.efrei.Util.Helper;
+import fr.efrei.domain.Category;
 
 public class CategoryFactory {
+    public static Category createCategory(int categoryId, String name) {
+        if (Helper.isStringValid(name) || Helper.isNumericEmpty(categoryId)) {
+            return null;
+        }
+        return new Category.Builder()
+                .setCategoryId(categoryId)
+                .setName(name)
+                .build();
+    }
 
 }
