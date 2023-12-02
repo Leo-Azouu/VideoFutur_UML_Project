372a4c8216ead4f7ae7d0fcad99dd6ac4dff96b8
TomHeising <104167231+TomHeising@users.noreply.github.com>
2023-12-02 23:35:02 +0200

    Add files via upload

---
+++ src/main/java/fr/efrei/factory/UserFactory.java
@@ -0,0 +1,18 @@
+package fr.efrei.factory;
+import fr.efrei.Util.Helper;
+import fr.efrei.domain.User;
+import java.util.*;
+
+public class UserFactory {
+    public static User createUser(int userId,String username, String password, String mail){
+        if(Helper.isNumericEmpty(userId)||Helper.isStringValid(username)||Helper.isStringValid(password)||Helper.isStringValid(mail)){
+            return null;
+        }
+        return new User.Builder()
+                .setUserId(userId)
+                .setUserName(username)
+                .setPassword(password)
+                .setMail(mail)
+                .build();
+    }
+}
