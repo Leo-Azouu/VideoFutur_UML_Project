7e08ff67b8c5ebd31088097ab43aa3120dd9a7ca
Leo-Azouu <104171733+Leo-Azouu@users.noreply.github.com>
2023-12-04 18:10:32 +0100

    CorrectTheFewMinorsMistakes
    good job guys no so much errors

--- src/main/java/fr/efrei/domain/Rental.java
+++ src/main/java/fr/efrei/domain/Rental.java
@@ -2,6 +2,8 @@ package fr.efrei.domain;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Rental {
@@ -28,6 +30,10 @@ public class Rental {
         return rentalDate;
     }
 
+    // just for test late date public void setRentalDate(Date rentalDate) {
+    //    this.rentalDate = rentalDate;
+    //}
+
     public Date getReturnDate() {
         return returnDate;
     }
@@ -35,18 +41,66 @@ public class Rental {
     public Movie getRentedMovie() {
         return rentedMovie;
     }
+    public void adjustReturnDate(int daysToAdd) {
+        LocalDate adjustedReturnDate = LocalDate.now().plusDays(daysToAdd);
+        this.returnDate = Date.from(adjustedReturnDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    }
+    public double calculateRentalFee() {
+        if (returnDate != null) {
+            long daysRented = ChronoUnit.DAYS.between(rentalDate.toInstant(), returnDate.toInstant());
+            // Exemple simple : 1 euro par jour de location
+            double baseFeePerDay = 3.50;
 
-    public double calculateRentalFee(){
-        return 0.0;
+            // Si le retour est en retard, appliquez un tarif différent
+            if (isLate()) {
+                double lateFeePerDay = 5.0; // Modifiez ceci selon votre logique de tarification pour les retards
+                return (baseFeePerDay + lateFeePerDay) * daysRented;
+            } else {
+                return baseFeePerDay * daysRented;
+            }
+        } else {
+            // La location est toujours en cours
+            System.out.println("Movie not returned yet.");
+            return 0.0;
+        }
     }
 
-    public void processReturn(){
+    public void processReturn() {
+        if (returnDate == null) {
+            returnDate = new Date();
+            System.out.println("Movie returned on: " + returnDate);
 
+            double rentalFee;
+
+            if (isLate()) {
+                rentalFee = calculateRentalFee();
+                System.out.println("Late return. Additional fee applied: " + rentalFee);
+            } else {
+                rentalFee = rentedMovie.getRentalRate();
+                System.out.println("On-time return. Standard rental fee: " + rentalFee);
+            }
+
+            // Affichez le montant total sans effectuer de paiement
+            System.out.println("Total amount to pay: " + rentalFee + " $");
+
+        } else {
+            System.out.println("Movie already returned on: " + returnDate);
+        }
     }
 
-    public boolean isLate(){
-        LocalDate localReturnDate = returnDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
-        return LocalDate.now().isAfter(localReturnDate);
+    public boolean isLate() {
+        if (returnDate != null) {
+            // Si la date de retour est après la date prévue
+            return returnDate.after(calculateDueDate());
+        }
+        // Si le film n'a pas encore été retourné, il n'est pas en retard
+        return false;
+    }
+    public Date calculateDueDate() {
+        Calendar dueDateCalendar = Calendar.getInstance();
+        dueDateCalendar.setTime(rentalDate);
+        dueDateCalendar.add(Calendar.DAY_OF_MONTH, 7); // Ajoutez 7 jours à la date de location
+        return dueDateCalendar.getTime();
     }
 
     public static class Builder{
