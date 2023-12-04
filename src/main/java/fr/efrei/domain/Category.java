9ac9750932f47e39b85d2ebca710e4b8c3cd77df
Leo-Azouu <104171733+Leo-Azouu@users.noreply.github.com>
2023-12-04 18:02:27 +0100

    UpdateCategory
    Add few utility with arrayList

--- src/main/java/fr/efrei/domain/Category.java
+++ src/main/java/fr/efrei/domain/Category.java
@@ -1,4 +1,5 @@
 package fr.efrei.domain;
+import java.util.ArrayList;
 import java.util.List;
 public class Category {
     private int categoryId;
@@ -7,6 +8,7 @@ public class Category {
 
     private Category() {
     }
+
     public int getCategoryId() {
         return categoryId;
     }
@@ -20,29 +22,35 @@ public class Category {
     }
 
     public void addMovie(Movie movie) {
+        if (movies == null) {
+            movies = new ArrayList<>();
+        }
         movies.add(movie);
         System.out.println("Movie added to the category successfully");
     }
+
     public void removeMovie(Movie movie) {
         if (movies.remove(movie))
             System.out.println("Movie removed from the category successfully");
         else
             System.out.println("Movie not found in the category");
     }
+
     @Override
     public String toString() {
         return "Category{" +
                 "categoryId=" + categoryId +
                 ", name='" + name + '\'' +
-                ", movies=" + movies +
+                ", movies=" + (movies != null ? movies.toString() : "null") +
                 '}';
     }
 
     private Category(Builder builder) {
         this.categoryId = builder.categoryId;
         this.name = builder.name;
-        this.movies = builder.movies;
+        this.movies = builder.movies != null ? new ArrayList<>(builder.movies) : new ArrayList<>();
     }
+
     public static class Builder {
         private int categoryId;
         private String name;
@@ -52,24 +60,26 @@ public class Category {
             this.categoryId = categoryId;
             return this;
         }
-        public Builder setName(String name){
+
+        public Builder setName(String name) {
             this.name = name;
             return this;
         }
-        public Builder setMovies(List<Movie> movies){
+
+        public Builder setMovies(List<Movie> movies) {
             this.movies = movies;
             return this;
         }
+
         public Builder copy(Category category) {
             this.categoryId = category.categoryId;
             this.name = category.name;
             this.movies = category.movies;
             return this;
         }
+
         public Category build() {
             return new Category(this);
         }
     }
-
-
 }
