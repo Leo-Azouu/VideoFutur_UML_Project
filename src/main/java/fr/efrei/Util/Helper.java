326f5d3c8f032b2ddecb9d9888ed8cd4b56137a0
Leo-Azouu <104171733+Leo-Azouu@users.noreply.github.com>
2023-12-04 18:22:32 +0100

    LastHelperChanges

--- src/main/java/fr/efrei/Util/Helper.java
+++ src/main/java/fr/efrei/Util/Helper.java
@@ -1,11 +1,8 @@
 package fr.efrei.Util;
 
-import fr.efrei.domain.Category;
-
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 public class Helper {
     public static Boolean isStringValid(String str) {
@@ -38,10 +35,6 @@ public class Helper {
             return true;
         }
     }
-    public static <T> boolean isListEmpty(List<T> list) {
-        return list == null || list.isEmpty();
-    }
-
 public static Date parseStringToDate(String date) {
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
         Date parsedDate = null;
